7a8540803bd01ceccb76278f9fcba7e311094aee
Cleophas <cleophasfoy@gmail.com>
2022-09-26 14:16:27 -0500

    added some game rules logic

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ document.addEventListener('DOMContentLoaded', () => {
   const grid = document.querySelector('.grid')
   const width = 8
   const squares = []
+  let score = 0
 
 
   const candyColors = [
@@ -72,7 +73,64 @@ document.addEventListener('DOMContentLoaded', () => {
 
   function dragEnd() {
     console.log(this.id, 'dragend')
+    //what is a valid move?
+    let validMoves = [
+      squareIdBeingDragged -1,
+      squareIdBeingDragged -width,
+      squareIdBeingDragged +1,
+      squareIdBeingDragged +width
+    ]
+    let validMove = validMoves.includes(squareIdBeingReplaced)
+
+    if (squareIdBeingReplaced && validMove) {
+      squareIdBeingReplaced = null
+    } else if (squareIdBeingReplaced && !validMove) {
+      squares[squareIdBeingReplaced].style.backgroundColor = colorBeingReplaced
+      squares[squareIdBeingDragged].style.backgroundColor = colorBeingDragged
+    } else squares[squareIdBeingDragged].style.backgroundColor = colorBeingDragged
   }
+
+
+  //Checking for matches
+  //check for row of three
+  function checkRowForThree() {
+    for (i = 0; i < 61; i++) {
+      let rowOfThree = [i, i+1, i+2]
+      let decidedColor = squares[i].style.backgroundColor
+      const isBlank = squares[i].style.backgroundColor === ''
+      const notValid = [6, 7, 14, 15, 22, 23, 30, 31, 38, 39, 46, 47, 54, 55]
+      if (notValid.includes(i)) continue
+
+      if (rowOfThree.every(index => squares[index].style.backgroundColor === decidedColor && !isBlank)) {
+        score += 3
+        rowOfThree.forEach(index => {
+          squares[index].style.backgroundColor = ''
+        })
+      }
+    }
+  }
+  checkRowForThree()
+
+  //check for column of three
+  function checkColumnForThree() {
+    for (i = 0; i < 47; i++) {
+      let columnOfThree = [i, i+width, i+ (2 * width)]
+      let decidedColor = squares[i].style.backgroundColor
+      const isBlank = squares[i].style.backgroundColor === ''
+      if (columnOfThree.every(index => squares[index].style.backgroundColor === decidedColor && !isBlank)) {
+        score += 3
+        columnOfThree.forEach(index => {
+          squares[index].style.backgroundColor = ''
+        })
+      }
+    }
+  }
+  checkColumnForThree()
+
+  window.setInterval(function() {
+    checkRowForThree()
+    checkColumnForThree()
+  }, 100)
 
 
 
